4ccc0a5b8489ce4f5416640d3cd49040bc1a0149
Daniel Ceballos <danielceballos94@gmail.com>
2023-12-09 19:47:55 -0500

    add basic banner css

--- blocks/banner/banner.js
+++ blocks/banner/banner.js
@@ -1,21 +1,25 @@
 import { createOptimizedPicture } from "../../scripts/aem.js";
 
 export default function decorate(block) {
-//   const cols = [...block.firstElementChild.children];
-//   block.classList.add(`columns-${cols.length}-cols`);
+  const items = [...block.firstElementChild.children];
+  console.log('items in block ', items)
+  const bannerImgDiv = document.querySelector('.banner picture');
+  console.log('banner img ', bannerImgDiv)
+  //   const cols = [...block.firstElementChild.children];
+  //   block.classList.add(`columns-${cols.length}-cols`);
 
-//   // setup image columns
-//   [...block.children].forEach((row) => {
-//     [...row.children].forEach((col) => {
-//       const pic = col.querySelector("picture");
-//       if (pic) {
-//         const picWrapper = pic.closest("div");
-//         if (picWrapper && picWrapper.children.length === 1) {
-//           // picture is only content in column
-//           picWrapper.classList.add("columns-img-col");
-//         }
-//       }
-//     });
-//   });
-    console.log('block is here ', block)
+  //   // setup image columns
+  //   [...block.children].forEach((row) => {
+  //     [...row.children].forEach((col) => {
+  //       const pic = col.querySelector("picture");
+  //       if (pic) {
+  //         const picWrapper = pic.closest("div");
+  //         if (picWrapper && picWrapper.children.length === 1) {
+  //           // picture is only content in column
+  //           picWrapper.classList.add("columns-img-col");
+  //         }
+  //       }
+  //     });
+  //   });
+  console.log("block is here ", block);
 }
